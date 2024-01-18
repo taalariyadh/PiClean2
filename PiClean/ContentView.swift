80e9090fa35e0e7da03a934ca0641586d35b9cd0
Reema Alfaleh <reemabf2@gmail.com>
2024-01-18 15:41:42 +0300

    Initial Commit

---
+++ PiClean/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PiClean
+//
+//  Created by Reema Alfaleh on 06/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PiClean/PiCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiCleanApp.swift
+//  PiClean
+//
+//  Created by Reema Alfaleh on 06/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PiCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
